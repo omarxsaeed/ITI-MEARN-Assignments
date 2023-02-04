c604ccd0e3c4d54566dd09c94cde7e09bef9b518
xOmarSaeed <omar.ahmed.saeed.1999@gmail.com>
2023-02-04 21:36:13 +0200

    Angular Day 2

--- Angular/Angular-App/src/app/Components/products/products.component.ts
+++ Angular/Angular-App/src/app/Components/products/products.component.ts
@@ -12,6 +12,7 @@ import {
   ICategory,
   DiscountOffers,
 } from '../../SharedClassesandtypes/category';
+import { ProductsService } from 'src/app/Services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -19,7 +20,7 @@ import {
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit, OnChanges {
-  private productList: IProduct[];
+  // private productList: IProduct[];
   categorizedProductList: IProduct[] = [];
   @Input() receivedCategoryID: number = 0;
   clientName: String;
@@ -27,111 +28,29 @@ export class ProductsComponent implements OnInit, OnChanges {
   orderTotalPrice: number = 0;
   @Output() onTotalPriceChange: EventEmitter<number>;
   purchaseDate: Date = new Date();
-  constructor() {
+  constructor(private prdService: ProductsService) {
     this.onTotalPriceChange = new EventEmitter<number>();
-    this.productList = [
-      {
-        id: 1,
-        name: 'Lenvo thinpad X230',
-        price: 100,
-        quantity: 2,
-        categoryID: 1,
-        imgURL: '',
-      },
-      {
-        id: 2,
-        name: 'Dell',
-        price: 200,
-        quantity: 20,
-        categoryID: 1,
-        imgURL: 'https://picsum.photos/200',
-      },
-      {
-        id: 3,
-        name: 'Lenovo Tab',
-        price: 100,
-        quantity: 10,
-        categoryID: 2,
-        imgURL: 'https://picsum.photos/200',
-      },
-      {
-        id: 4,
-        name: 'Samsung Tab',
-        price: 100,
-        quantity: 10,
-        categoryID: 2,
-        imgURL: 'https://picsum.photos/200',
-      },
-      {
-        id: 5,
-        name: 'Samsung note 10',
-        price: 100,
-        quantity: 10,
-        categoryID: 3,
-        imgURL: 'https://picsum.photos/200',
-      },
-      {
-        id: 6,
-        name: 'Samsung S10',
-        price: 100,
-        quantity: 10,
-        categoryID: 3,
-        imgURL: 'https://picsum.photos/200',
-      },
-      {
-        id: 7,
-        name: 'tochiba Utlra',
-        price: 60000,
-        quantity: 14,
-        imgURL: 'https://picsum.photos/200',
-        categoryID: 4,
-      },
-      {
-        id: 8,
-        name: 'Apple Utlra',
-        price: 7800060,
-        quantity: 6,
-        imgURL: 'https://picsum.photos/200',
-        categoryID: 4,
-      },
-      {
-        id: 9,
-        name: 'test Utlra',
-        price: 7800060,
-        quantity: 3,
-        imgURL: 'https://picsum.photos/200',
-        categoryID: 4,
-      },
-      {
-        id: 10,
-        name: 'jhgfjxz',
-        imgURL: 'https://picsum.photos/200',
-        quantity: 33,
-        price: 44444444,
-        categoryID: 1,
-      },
-      {
-        name: 'fsdjhfgsdjhkgfsh',
-        price: 7800060,
-        quantity: 3,
-        imgURL: 'https://picsum.photos/200',
-        categoryID: 4,
-        id: 11,
-      },
-    ];
     this.clientName = 'Anas';
     this.isPurchased = false;
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.categorizedProductList = this.prdService.getProductsByCategoryID(
+      this.receivedCategoryID
+    );
+  }
   ngOnChanges(changes: SimpleChanges): void {
-    if (this.receivedCategoryID == 0) {
-      this.categorizedProductList = this.productList;
-    } else {
-      this.categorizedProductList = [];
-      this.categorizedProductList = this.productList.filter(
-        (product) => product.categoryID == this.receivedCategoryID
-      );
-    }
+    this.categorizedProductList = this.prdService.getProductsByCategoryID(
+      this.receivedCategoryID
+    );
+
+    // if (this.receivedCategoryID == 0) {
+    //   this.categorizedProductList = this.productList;
+    // } else {
+    //   this.categorizedProductList = [];
+    //   this.categorizedProductList = this.productList.filter(
+    //     (product) => product.categoryID == this.receivedCategoryID
+    //   );
+    // }
   }
 
   // update price and reduce stock
---
+++ Unit Testing/Day 2/MockSpyTask.spec.js
@@ -0,0 +1,18 @@
+var counter = {
+  currentValues: function () {
+    return 1;
+  },
+};
+function sumOfValues() {
+ return counter.currentValues();
+}
+
+describe("mock and spy", function () {
+ 
+  // and expect that we called the spy function for one time
+  it("spying", function () {
+   
+
+  });
+
+});
